c37f6f6ae488b1b2f39bffad1e1782f122581d8c
Ed <108343328+eded001@users.noreply.github.com>
2025-12-12 13:53:45 -0300

    feat: add a basic frontend project

---
+++ basic-frontend/app/src/App.tsx
@@ -0,0 +1,19 @@
+export default function App() {
+  return (
+    <main className="h-screen bg-linear-to-br from-zinc-900 via-zinc-600 to-zinc-300 flex items-center justify-center p-6">
+      <h1
+        className="
+          text-7xl
+          font-extrabold
+          text-white
+          drop-shadow-[0_0_15px_rgba(255,255,255,0.35)]
+          tracking-wide
+          animate-pulse
+          select-none
+        "
+      >
+        Hello World
+      </h1>
+    </main>
+  );
+}
